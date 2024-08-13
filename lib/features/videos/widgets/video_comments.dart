aa72b7e943acc1c2f6fd9ed2308d07f873b964d9
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-08-13 21:14:46 +0900

    text input actions

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -11,13 +11,29 @@ class VideoComments extends StatefulWidget {
 }
 
 class _VideoCommentsState extends State<VideoComments> {
+  bool _isWriting = false;
+
   void _onClosePressed() {
     Navigator.of(context).pop();
+  }
+
+  void _stopWriting() {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _isWriting = false;
+    });
+  }
+
+  void _onStartWriting() {
+    setState(() {
+      _isWriting = true;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
     return Container(
       height: size.height * 0.7,
       clipBehavior: Clip.hardEdge,
@@ -40,100 +56,143 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: Stack(
-          children: [
-            ListView.separated(
-              padding: const EdgeInsets.symmetric(
-                vertical: Sizes.size10,
-                horizontal: Sizes.size16,
-              ),
-              separatorBuilder: (context, index) => Gaps.v20,
-              itemCount: 10,
-              itemBuilder: (context, index) => Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const CircleAvatar(
-                    radius: 18,
-                    child: Text("감자"),
-                  ),
-                  Gaps.h10,
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "감자",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: Sizes.size14,
-                            color: Colors.grey.shade500,
-                          ),
-                        ),
-                        Gaps.v4,
-                        const Text(
-                            "That's not it l'v seen the same thing but also in a cave"),
-                      ],
-                    ),
-                  ),
-                  Gaps.h10,
-                  Column(
-                    children: [
-                      FaIcon(
-                        FontAwesomeIcons.heart,
-                        size: Sizes.size20,
-                        color: Colors.grey.shade500,
-                      ),
-                      Gaps.v2,
-                      Text(
-                        "52.2K",
-                        style: TextStyle(
-                          color: Colors.grey.shade500,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              width: size.width,
-              child: BottomAppBar(
-                color: Colors.white,
-                child: Row(
+        body: GestureDetector(
+          onTap: _stopWriting,
+          child: Stack(
+            children: [
+              ListView.separated(
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size10,
+                  horizontal: Sizes.size16,
+                ),
+                separatorBuilder: (context, index) => Gaps.v20,
+                itemCount: 10,
+                itemBuilder: (context, index) => Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    CircleAvatar(
+                    const CircleAvatar(
                       radius: 18,
-                      backgroundColor: Colors.grey.shade500,
-                      foregroundColor: Colors.white,
-                      child: const Text("감자"),
+                      child: Text("감자"),
                     ),
                     Gaps.h10,
                     Expanded(
-                      child: TextField(
-                        cursorColor: Theme.of(context).primaryColor,
-                        decoration: InputDecoration(
-                          hintText: "Write a comment...",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(
-                              Sizes.size12,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "감자",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: Sizes.size14,
+                              color: Colors.grey.shade500,
                             ),
-                            borderSide: BorderSide.none,
                           ),
-                          filled: true,
-                          fillColor: Colors.grey.shade200,
-                          contentPadding: const EdgeInsets.symmetric(
-                            horizontal: Sizes.size12,
-                            vertical: Sizes.size12,
-                          ),
-                        ),
+                          Gaps.v4,
+                          const Text(
+                              "That's not it l'v seen the same thing but also in a cave"),
+                        ],
                       ),
                     ),
+                    Gaps.h10,
+                    Column(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size20,
+                          color: Colors.grey.shade500,
+                        ),
+                        Gaps.v2,
+                        Text(
+                          "52.2K",
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                          ),
+                        ),
+                      ],
+                    )
                   ],
                 ),
               ),
-            )
-          ],
+              Positioned(
+                bottom: 0,
+                width: size.width,
+                child: BottomAppBar(
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 18,
+                        backgroundColor: Colors.grey.shade500,
+                        foregroundColor: Colors.white,
+                        child: const Text("감자"),
+                      ),
+                      Gaps.h10,
+                      Expanded(
+                        child: SizedBox(
+                          height: Sizes.size52,
+                          child: TextField(
+                            onTap: _onStartWriting,
+                            expands: true,
+                            minLines: null,
+                            maxLines: null,
+                            textInputAction: TextInputAction.newline,
+                            cursorColor: Theme.of(context).primaryColor,
+                            decoration: InputDecoration(
+                              hintText: "Write a comment...",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(
+                                  Sizes.size12,
+                                ),
+                                borderSide: BorderSide.none,
+                              ),
+                              filled: true,
+                              fillColor: Colors.grey.shade200,
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: Sizes.size12,
+                              ),
+                              suffixIcon: Padding(
+                                padding: const EdgeInsets.only(
+                                  right: Sizes.size14,
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    FaIcon(
+                                      FontAwesomeIcons.at,
+                                      color: Colors.grey.shade900,
+                                    ),
+                                    Gaps.h14,
+                                    FaIcon(
+                                      FontAwesomeIcons.gift,
+                                      color: Colors.grey.shade900,
+                                    ),
+                                    Gaps.h14,
+                                    FaIcon(
+                                      FontAwesomeIcons.faceSmile,
+                                      color: Colors.grey.shade900,
+                                    ),
+                                    Gaps.h14,
+                                    if (_isWriting)
+                                      GestureDetector(
+                                        onTap: _stopWriting,
+                                        child: FaIcon(
+                                          FontAwesomeIcons.circleArrowUp,
+                                          color: Theme.of(context).primaryColor,
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
