eb494a24d4fbdc47385278e31ce888c6afbeb3ea
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-08-16 13:37:51 +0900

    Dismissible

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+import 'package:tiktok_clone/features/inbox/activity_screen.dart';
 import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
 import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
@@ -42,7 +43,8 @@ class TikTokApp extends StatelessWidget {
           )),
       // home: const SignUpScreen(),
       // home: const InterestsScreen(),
-      home: const MainNavigationScreen(),
+      // home: const MainNavigationScreen(),
+      home: const ActivityScreen(),
     );
   }
 }
