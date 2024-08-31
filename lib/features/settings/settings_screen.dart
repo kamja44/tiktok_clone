8e1f709aff6edbf3654b81627a0a3129e16838e0
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-08-31 20:25:37 +0900

    AboutListTile widget , showAboutDialog function 둘이 같은 기능인데 showAboutDialog은 수동으로 만들고 AboutListTile은 위젯 호출 시 자동으로 됨.

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -10,14 +10,23 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Settings"),
       ),
-      body: const Column(
+      body: ListView(
         children: [
-          CupertinoActivityIndicator(
-            radius: 40,
-            // animating: false,
+          ListTile(
+            onTap: () => showAboutDialog(
+              context: context,
+              applicationVersion: "1.0",
+              applicationLegalese: "All rights reserved. Please don't copy me.",
+            ),
+            title: const Text(
+              "About",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            subtitle: const Text("About this app....."),
           ),
-          CircularProgressIndicator(),
-          CircularProgressIndicator.adaptive(),
+          const AboutListTile(),
         ],
       ),
     );
