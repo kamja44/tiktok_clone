bd4cd5f25e291ce948f4582f652756917bcad9e8
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-08-31 20:40:34 +0900

    ShowDatePicker, ShowTimePicker, ShowDateRangePicker Widget

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -13,18 +13,37 @@ class SettingsScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            onTap: () => showAboutDialog(
-              context: context,
-              applicationVersion: "1.0",
-              applicationLegalese: "All rights reserved. Please don't copy me.",
-            ),
-            title: const Text(
-              "About",
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            subtitle: const Text("About this app....."),
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+              );
+              print("ㄴ $date");
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print("ㄴ $time");
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                      data: ThemeData(
+                        appBarTheme: const AppBarTheme(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.black,
+                        ),
+                      ),
+                      child: child!);
+                },
+              );
+              print("ㄴ $booking");
+            },
+            title: const Text("What is your birthday?"),
           ),
           const AboutListTile(),
         ],
