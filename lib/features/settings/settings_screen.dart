2f13534ee13d9a5c85b52223e3396c488408664c
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-08-31 20:16:18 +0900

    Loading Widget

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -9,25 +10,14 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Settings"),
       ),
-      body: ListWheelScrollView(
-        diameterRatio: 0.9,
-        itemExtent: 200,
+      body: const Column(
         children: [
-          for (var x in [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Container(
-                color: Colors.teal,
-                alignment: Alignment.center,
-                child: const Text(
-                  "Pick me",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 39,
-                  ),
-                ),
-              ),
-            )
+          CupertinoActivityIndicator(
+            radius: 40,
+            // animating: false,
+          ),
+          CircularProgressIndicator(),
+          CircularProgressIndicator.adaptive(),
         ],
       ),
     );
